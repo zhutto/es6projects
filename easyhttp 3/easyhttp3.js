6dc8a58441330cf1bdea7c77eea4a79dc7c8b4ec
zhutto <35507425+zhutto@users.noreply.github.com>
2018-07-27 15:26:14 -0600

    Add files via upload
    Same project with async await replacing promises

---
+++ easyhttp 3/easyhttp3.js
@@ -0,0 +1,53 @@
+class EasyHTTP {
+
+// make get request
+
+  async get(url) {
+  const response = await fetch (url);
+
+  const resData = await response.json();
+  return resData;
+  }
+
+
+  // make post request
+
+  async post(url, data) {
+  const responbe = await fetch(url, {
+    method: 'POST',
+    headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+      });
+    const resData = await response.json();
+    return resData;
+  }
+
+// make put request
+
+async put(url, data) {
+    const response = await fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    const resData = await response.json();
+    return resData;
+  }
+
+// delete request
+
+async delete(url) {
+  const repsonse = await fetch(url, {
+    method: 'DELETE',
+    headers: {
+      'Content-type': 'application/json'
+    }
+});
+const resData = await  'deleted';
+return resData;
+}
+}
